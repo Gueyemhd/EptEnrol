c52245232ed02ac20140291d9a0136407dff39ce
TSTCodersRoom <86240458+TSTCodersRoom@users.noreply.github.com>
2023-05-06 21:42:07 +0000

    TST Branch

--- eptEnrol/src/app/app.module.ts
+++ eptEnrol/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserInfosComponent } from './user-infos/user-infos.component';
+import { ListeCandidatsComponent } from './liste-candidats/liste-candidats.component';
+import { InscriptionConcoursComponent } from './inscription-concours/inscription-concours.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserInfosComponent,
+    ListeCandidatsComponent,
+    InscriptionConcoursComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
